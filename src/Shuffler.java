670645744590c18861fa191f4b81db1df015ee2f
Ritvik Reddy Durgempudi <durgempudirr@s.dcsdk12.org>
2019-04-09 14:28:24 -0600

    Finish code for a3

--- src/Shuffler.java
+++ src/Shuffler.java
@@ -7,7 +7,7 @@ public class Shuffler {
 	 * The number of consecutive shuffle steps to be performed in each call
 	 * to each sorting procedure.
 	 */
-	private static final int SHUFFLE_COUNT = 1;
+	private static final int SHUFFLE_COUNT = 3;
 
 
 	/**
@@ -51,6 +51,23 @@ public class Shuffler {
 	 */
 	public static void perfectShuffle(int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int[] shuffled = new int[values.length];
+		int k = 0;
+		for (int j = 0; j < values.length/2; j++){
+			shuffled[k] = values[j];
+			k += 2;
+		}
+		k = 1;
+
+		for(int j = values.length/2; j < values.length; j++){
+			shuffled[k] = values[j];
+			k += 2;
+		}
+
+		for (int w = 0; w < values.length; w++){
+			values[w] = shuffled[w];
+		}
+
 	}
 
 	/**
@@ -66,5 +83,13 @@ public class Shuffler {
 	 */
 	public static void selectionShuffle(int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		for(int k = 0; k < values.length; k++){
+			int j = (int) Math.random() * values.length;
+			int temp = values[k];
+			values[k] = values[j];
+			values[j] = temp;
+			
+			
+		}
 	}
 }
